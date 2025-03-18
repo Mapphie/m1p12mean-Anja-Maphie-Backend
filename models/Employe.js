82e28c5f5f60c4e6dc432b734b32e29ab2a53602
Mapphie <maphiesarobidyrazafindrakotoso@gmail.com>
2025-03-18 22:42:06 +0300

    ajouter date dans le model employe

--- models/Employe.js
+++ models/Employe.js
@@ -4,6 +4,7 @@ const EmployeSchema = new mongoose.Schema({
     nom: {type: String, required: true},
     email: {type: String, required: true},
     contact: {type: String, required: true},
+    adresse: {type: String, required: true},
     poste: {type: String, required: true},
     date: {type: Date, required: true},
     salaire: {type: Number, required: true},
