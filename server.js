264c1ab3076172e43e86df9637396e10b0b05144
anjaNaly <randriamanalinaanja@gmail.com>
2025-04-03 09:41:46 +0300

    Revert "ajout du lien front pour la session"
    This reverts commit b665ab7085885aff3985b0efb17d7f1a3a5f9ed7.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 //Middleware
 app.use(cors({
-    origin: 'https://m1p12mean-anja-maphie.netlify.app/',
+    origin: 'http://localhost:4200',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true
