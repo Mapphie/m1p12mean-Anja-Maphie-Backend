b665ab7085885aff3985b0efb17d7f1a3a5f9ed7
anjaNaly <randriamanalinaanja@gmail.com>
2025-04-03 09:20:32 +0300

    ajout du lien front pour la session

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 //Middleware
 app.use(cors({
-    origin: 'http://localhost:4200',
+    origin: 'https://m1p12mean-anja-maphie.netlify.app/',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true
