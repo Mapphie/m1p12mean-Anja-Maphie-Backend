68c990ebdeac71c2f109a1c83cd054ace9f398c9
anjaNaly <randriamanalinaanja@gmail.com>
2025-04-03 17:20:42 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,8 +3,6 @@ const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     nom: { type: String, required: true },
-    prenom: { type: String, required: true },
-    // idrole: { type: String, required: true },
     idrole: { 
         _idrole: { type: String, required: true },
         role: { type: String, required: true }
