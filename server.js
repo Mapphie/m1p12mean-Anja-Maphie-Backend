0d8aaf87fde2b7367f95736bb3135f80f4ebef8b
anjaNaly <randriamanalinaanja@gmail.com>
2025-04-03 09:47:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 //Middleware
 app.use(cors({
-    origin: 'http://localhost:4200',
+    // origin: 'http://localhost:4200',
+    origin: 'https://m1p12mean-anja-maphie.netlify.app/',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true
